1a3a91dadb0ca4fbc5922d034bf8c6e67cc66d3a
seth <25378370+Seth52@users.noreply.github.com>
2022-11-09 19:23:07 -0500

    webpack restaurant

---
+++ src/index.js
@@ -0,0 +1,30 @@
+console.log('test');
+
+<body style="background-image:url('/src/background.png')">
+    <div id="content" style="background-color:rgb(255, 178, 62); overflow:auto; display:grid; margin-left:25%; margin-right: 25%; margin-top:5%; justify-content: center; border-radius: 5%; padding: 50px; padding-bottom: 225px; ">
+    
+        <div id="header" style="display:flex; align-items: center;"><img src="/src/hamm.png" alt="hammock" style="height:40px; width:40px; padding-right: 5px;">
+            <H1>Beerbellyz Bungalow</H1><img src="/src/hamm.png" alt="hammock" style="height:40px; width:40px; padding-left: 5px;"></div>
+
+            <div id="storestuff" style="display:flex; justify-content: space-between; height: 500px; flex-direction: column;">
+
+        <div id="info" style="border: 6px solid rgb(211, 120, 0); padding:5px; background-color: rgb(237, 243, 209); border-radius: 4px;"> Come thru BBz bungalow got dat good good</div>
+        <div id="hrs" style="border: 6px solid rgb(211, 120, 0); padding:5px; background-color: rgb(237, 243, 209); border-radius: 4px;">
+            <h3>Hours</h3>
+             <ul>sundeyz we open</ul>
+             <ul>mondeyz we open</ul>
+             <ul>tuesdeyz we open</ul>
+             <ul>wednesdeyyy baby</ul>
+             <ul>thursday we closed </ul>
+             <ul> friday we backkkk</ul>
+        
+        </div>
+        <div id="location" style="border:6px solid rgb(211, 120, 0); padding:5px; background-color: rgb(237, 243, 209); border-radius: 4px;">
+            <h3>Location</h3>
+            yo moms house
+        </div>
+
+
+    <!-- store stuff div -->
+    </div>
+    </div>
